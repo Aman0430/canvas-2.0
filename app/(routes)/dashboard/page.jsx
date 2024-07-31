5df03bc3c5122a28eb81dedbbf28b948e0954faf
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2024-07-31 23:24:32 +0530

    initial setup dashboard

---
+++ app/(routes)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../_components/Header";
+import WorkspaceList from "../dashboard/_components/WorkspaceList";
+
+function Dashboard() {
+  return (
+    <div>
+      <Header />
+      <WorkspaceList />
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ app/_components/Header.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { OrganizationSwitcher, UserButton, useAuth } from "@clerk/nextjs";
+import Logo from "./Logo";
+import React from "react";
+import { ThemeSwitcherBtn } from "@/components/ui/ThemeSwitcherBtn";
+import { Button } from "@/components/ui/button";
+
+function Header() {
+  const { orgId } = useAuth();
+  // console.log(orgId);
+  return (
+    <div className="flex items-center justify-between p-3 shadow-sm">
+      <Logo />
+      <div className="flex items-center gap-2">
+        <div className="dark:bg-secondary-foreground rounded-md p-[0.3rem]">
+          <OrganizationSwitcher
+            afterCreateOrganizationUrl={"/dashboard"}
+            afterLeaveOrganizationUrl={"/dashboard"}
+          />
+        </div>
+        <ThemeSwitcherBtn />
+        <UserButton />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
